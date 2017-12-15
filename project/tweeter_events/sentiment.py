497d0fbd228a08f49af98a478fc212e7a34e2c6d
Ergys Dona <errikosd@gmail.com>
2017-12-15 03:04:31 +0100

    project: add sentiment analysis snippet
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

---
+++ project/tweeter_events/sentiment.py
@@ -0,0 +1,41 @@
+from textblob import TextBlob
+# textblob classes for English
+from textblob.en.taggers import NLTKTagger as PatternTaggerEn
+from textblob.en.sentiments import PatternAnalyzer as PatternAnalyzerEn
+# textblob classes for French
+from textblob_fr import PatternTagger as PatternTaggerFr
+from textblob_fr import PatternAnalyzer as PatternAnalyzerFr
+# textblob classes for German
+from textblob_de import PatternTagger as PatternTaggerDe
+from textblob_de import PatternAnalyzer as PatternAnalyzerDe
+
+# instantiate objects
+objs = {
+  "en": {  # english
+    "pos_tagger": PatternTaggerEn(),
+    "analyzer": PatternAnalyzerEn(),
+  },
+  "fr": {  # french
+    "pos_tagger": PatternTaggerFr(),
+    "analyzer": PatternAnalyzerFr(),
+  },
+  "de": {  # german
+    "pos_tagger": PatternTaggerDe(),
+    "analyzer": PatternAnalyzerDe(),
+  }
+}
+
+samples = [
+  ("en", "I am very happy!"),
+  ("fr", "Je suis très heureux!"),
+  ("de", "Ich bin sehr glücklich!"),
+]
+
+for lang, text in samples:
+  blob = TextBlob(text, **objs[lang])
+  # polarity (double):
+  #   - (0.0, 1.0] positive
+  #   - (0.0) neutral
+  #   - [-1.0, 0) negative
+  polarity, subjectivity = blob.sentiment  # need to do it this way!
+  print(lang, polarity)
